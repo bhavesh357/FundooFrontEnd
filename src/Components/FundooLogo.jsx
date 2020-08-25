da48c75ec57b3561e3096354291de67cf7737b0d
bhavesh357 <bkadam357@gmail.com>
2020-08-26 01:44:39 +0530

    ADDED: fundoo logo

---
+++ src/Components/FundooLogo.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class FundooLogo extends React.Component{
+    render(){
+        return(
+            <div id="fundoo-logo">
+                <span id="logo-f">F</span><span id="logo-u">u</span><span id="logo-n">n</span><span id="logo-d">d</span><span id="logo-first-o">o</span><span id="logo-second-o">o</span>
+            </div>
+        );
+    }
+}
+
+export default FundooLogo;
--- src/Components/SignIn.jsx
+++ src/Components/SignIn.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import FundooLogo from './FundooLogo';
+import PageTitle from './PageTitle';
 
 class SignIn extends React.Component{
     render(){
         return(
-            <div>
-                Suup
+            <div id="sign-in">
+                <FundooLogo />
+                <PageTitle title="Sign in" />
             </div>
         );
     }
