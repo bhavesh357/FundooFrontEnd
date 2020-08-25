1e280ef88891a5cfe914515dcccaba67c3e26b94
bhavesh357 <bkadam357@gmail.com>
2020-08-26 00:38:32 +0530

    ADDED: signIn component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import SignIn from './Components/SignIn';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Switch>
+          <Route path='/' exact component={SignIn}></Route>
+          <Route path='/signUp' exact component={SignIn}></Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/Components/SignIn.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class SignIn extends React.Component{
+    render(){
+        return(
+            <div>
+                Suup
+            </div>
+        );
+    }
+}
+
+export default SignIn;
