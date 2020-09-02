4565a779306180d5accea132036e5112ae0fd7be
bhavesh357 <bkadam357@gmail.com>
2020-09-03 00:30:29 +0530

    ADDED: header and icon

--- src/Components/pages/Notes.jsx
+++ src/Components/pages/Notes.jsx
@@ -1,5 +1,7 @@
 import React from "react";
-import { Grid, Snackbar } from "@material-ui/core";
+import { Grid, Snackbar, AppBar, IconButton, Toolbar, Typography } from "@material-ui/core";
+import MenuIcon from '@material-ui/icons/Menu';
+import keepIcon from './../../Assets/keep.png';
 
 class Notes extends React.Component {
   state = {
@@ -29,7 +31,21 @@ class Notes extends React.Component {
           autoHideDuration={2000}
           message={this.state.snackbarMessage}
         />
-        
+        <Grid item md={12} container >
+            <AppBar className="app-bar">
+                <Toolbar className="header">
+                    <IconButton
+                        edge="start"
+                         >
+                        <MenuIcon className="menu-icon"/>
+                    </IconButton>
+                    <img src={keepIcon} alt="logo" className="keep-icon" />
+                    <Typography variant="h6" className="header-title" >
+                        Notes
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+        </Grid>
       </Grid>
     );
   }
