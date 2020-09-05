e8b79f7c5a16f2f9b189f725497c81afde87ef6d
bhavesh357 <bkadam357@gmail.com>
2020-09-05 11:42:16 +0530

    ADDED: plain notes with text to template

---
+++ src/Components/Notes.jsx
@@ -0,0 +1,134 @@
+import React from "react";
+import { Grid, Typography, Card } from "@material-ui/core";
+
+class Notes extends React.Component {
+
+  render() {
+    return (
+      <Grid container className="note">
+        <Grid item md={this.props.isDrawerOpen ? 12 : 10}>
+          <Grid container  spacing={2} className="note-row">
+            <Grid item md={3}>
+              <Card elevation={3} variant="outlined" className="note-card">
+                <Typography className="note-content">
+                  Nunc congue posuere lorem id mattis. Mauris id dolor ex.
+                  Mauris rhoncus id eros eu fermentum. Morbi semper, dui sit
+                  amet dapibus sollicitudin, massa lectus convallis metus, et
+                  mollis felis nunc vitae sem. Ut sit amet scelerisque urna.
+                  Nunc euismod quam in ex dignissim, et suscipit mi tempus.
+                  Maecenas sed ligula sit amet sem semper fermentum. Etiam
+                  dapibus aliquet enim, et sodales purus eleifend sit amet.
+                  Etiam pulvinar, felis a vulputate tempus, eros enim ultricies
+                  ipsum, id gravida tellus ante accumsan libero. Mauris
+                  fermentum lorem turpis, sit amet mattis est ultrices ut. Sed
+                  aliquam urna eu felis hendrerit, at blandit libero hendrerit.
+                  Cras ornare blandit elementum. Duis in fermentum ligula. Nunc
+                  id pharetra neque, quis dignissim nisi. Suspendisse mollis
+                  risus nec viverra viverra. Nulla fermentum lobortis ante,
+                  vitae lacinia libero pellentesque in. Vestibulum pretium,
+                  risus in dignissim condimentum, purus mi volutpat justo, id
+                  maximus urna neque vitae lacus. Suspendisse in velit malesuada
+                  velit rutrum venenatis sed a nisi. Nulla volutpat tortor sem,
+                  quis ultrices ligula ultrices in. Nunc eget ultrices quam.
+                  Nullam ut malesuada metus. Sed bibendum, dolor a fringilla
+                  vulputate, nunc augue gravida ipsum, eu pulvinar erat enim sed
+                  lacus. Maecenas vel dolor consequat, ullamcorper velit sed,
+                  suscipit ante. Aliquam erat volutpat
+                </Typography>
+              </Card>
+            </Grid>
+            <Grid item md={3}>
+              <Card elevation={3} variant="outlined" className="note-card">
+                <Typography className="note-content">
+                  Nunc congue posuere lorem id mattis. Mauris id dolor ex.
+                  Mauris rhoncus id eros eu fermentum. Morbi semper, dui sit
+                  amet dapibus sollicitudin, massa lectus convallis metus, et
+                  mollis felis nunc vitae sem. Ut sit amet scelerisque urna.
+                  Nunc euismod quam in ex dignissim, et suscipit mi tempus.
+                  Maecenas sed ligula sit amet sem semper fermentum. Etiam
+                  dapibus aliquet enim, et sodales purus eleifend sit amet.
+                  Etiam pulvinar, felis a vulputate tempus, eros enim ultricies
+                  ipsum, id gravida tellus ante accumsan libero. Mauris
+                  fermentum lorem turpis, sit amet mattis est ultrices ut. Sed
+                  aliquam urna eu felis hendrerit, at blandit libero hendrerit.
+                  Cras ornare blandit elementum. Duis in fermentum ligula. Nunc
+                  id pharetra neque, quis dignissim nisi. Suspendisse mollis
+                  risus nec viverra viverra. Nulla fermentum lobortis ante,
+                  vitae lacinia libero pellentesque in. Vestibulum pretium,
+                  risus in dignissim condimentum, purus mi volutpat justo, id
+                  maximus urna neque vitae lacus. Suspendisse in velit malesuada
+                  velit rutrum venenatis sed a nisi. Nulla volutpat tortor sem,
+                  quis ultrices ligula ultrices in. Nunc eget ultrices quam.
+                  Nullam ut malesuada metus. Sed bibendum, dolor a fringilla
+                  vulputate, nunc augue gravida ipsum, eu pulvinar erat enim sed
+                  lacus. Maecenas vel dolor consequat, ullamcorper velit sed,
+                  suscipit ante. Aliquam erat volutpat
+                </Typography>
+              </Card>
+            </Grid>
+            <Grid item md={3}>
+              <Card elevation={3} variant="outlined" className="note-card">
+                <Typography className="note-content">
+                  Nunc congue posuere lorem id mattis. Mauris id dolor ex.
+                  Mauris rhoncus id eros eu fermentum. Morbi semper, dui sit
+                  amet dapibus sollicitudin, massa lectus convallis metus, et
+                  mollis felis nunc vitae sem. Ut sit amet scelerisque urna.
+                  Nunc euismod quam in ex dignissim, et suscipit mi tempus.
+                  Maecenas sed ligula sit amet sem semper fermentum. Etiam
+                  dapibus aliquet enim, et sodales purus eleifend sit amet.
+                  Etiam pulvinar, felis a vulputate tempus, eros enim ultricies
+                  ipsum, id gravida tellus ante accumsan libero. Mauris
+                  fermentum lorem turpis, sit amet mattis est ultrices ut. Sed
+                  aliquam urna eu felis hendrerit, at blandit libero hendrerit.
+                  Cras ornare blandit elementum. Duis in fermentum ligula. Nunc
+                  id pharetra neque, quis dignissim nisi. Suspendisse mollis
+                  risus nec viverra viverra. Nulla fermentum lobortis ante,
+                  vitae lacinia libero pellentesque in. Vestibulum pretium,
+                  risus in dignissim condimentum, purus mi volutpat justo, id
+                  maximus urna neque vitae lacus. Suspendisse in velit malesuada
+                  velit rutrum venenatis sed a nisi. Nulla volutpat tortor sem,
+                  quis ultrices ligula ultrices in. Nunc eget ultrices quam.
+                  Nullam ut malesuada metus. Sed bibendum, dolor a fringilla
+                  vulputate, nunc augue gravida ipsum, eu pulvinar erat enim sed
+                  lacus. Maecenas vel dolor consequat, ullamcorper velit sed,
+                  suscipit ante. Aliquam erat volutpat
+                </Typography>
+              </Card>
+            </Grid>
+            <Grid item md={3}>
+              <Card elevation={3} variant="outlined" className="note-card">
+                <Typography className="note-content">
+                  Nunc congue posuere lorem id mattis. Mauris id dolor ex.
+                  Mauris rhoncus id eros eu fermentum. Morbi semper, dui sit
+                  amet dapibus sollicitudin, massa lectus convallis metus, et
+                  mollis felis nunc vitae sem. Ut sit amet scelerisque urna.
+                  Nunc euismod quam in ex dignissim, et suscipit mi tempus.
+                  Maecenas sed ligula sit amet sem semper fermentum. Etiam
+                  dapibus aliquet enim, et sodales purus eleifend sit amet.
+                  Etiam pulvinar, felis a vulputate tempus, eros enim ultricies
+                  ipsum, id gravida tellus ante accumsan libero. Mauris
+                  fermentum lorem turpis, sit amet mattis est ultrices ut. Sed
+                  aliquam urna eu felis hendrerit, at blandit libero hendrerit.
+                  Cras ornare blandit elementum. Duis in fermentum ligula. Nunc
+                  id pharetra neque, quis dignissim nisi. Suspendisse mollis
+                  risus nec viverra viverra. Nulla fermentum lobortis ante,
+                  vitae lacinia libero pellentesque in. Vestibulum pretium,
+                  risus in dignissim condimentum, purus mi volutpat justo, id
+                  maximus urna neque vitae lacus. Suspendisse in velit malesuada
+                  velit rutrum venenatis sed a nisi. Nulla volutpat tortor sem,
+                  quis ultrices ligula ultrices in. Nunc eget ultrices quam.
+                  Nullam ut malesuada metus. Sed bibendum, dolor a fringilla
+                  vulputate, nunc augue gravida ipsum, eu pulvinar erat enim sed
+                  lacus. Maecenas vel dolor consequat, ullamcorper velit sed,
+                  suscipit ante. Aliquam erat volutpat
+                </Typography>
+              </Card>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+    );
+  }
+}
+
+export default Notes;
