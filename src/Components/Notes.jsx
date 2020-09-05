4586c15bb521ba06e2fc31febffe050f3ef6ed3a
bhavesh357 <bkadam357@gmail.com>
2020-09-06 03:26:54 +0530

    ADDED: buttons which are only visible when hovered over

--- src/Components/Notes.jsx
+++ src/Components/Notes.jsx
@@ -13,12 +13,15 @@ import {
   CardHeader,
   CardActions,
 } from "@material-ui/core";
-import AddAlertOutlinedIcon from '@material-ui/icons/AddAlertOutlined';
-import PersonAddOutlinedIcon from '@material-ui/icons/PersonAddOutlined';
-import ColorLensOutlinedIcon from '@material-ui/icons/ColorLensOutlined';
-import CropOriginalIcon from '@material-ui/icons/CropOriginal';
-import MoreVertIcon from '@material-ui/icons/MoreVert';
-import ArchiveOutlinedIcon from '@material-ui/icons/ArchiveOutlined';
+import AddAlertOutlinedIcon from "@material-ui/icons/AddAlertOutlined";
+import PersonAddOutlinedIcon from "@material-ui/icons/PersonAddOutlined";
+import ColorLensOutlinedIcon from "@material-ui/icons/ColorLensOutlined";
+import CropOriginalIcon from "@material-ui/icons/CropOriginal";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import CheckCircleIcon from '@material-ui/icons/CheckCircle';
+import RoomOutlinedIcon from '@material-ui/icons/RoomOutlined';
+import RoomIcon from '@material-ui/icons/Room';
+import ArchiveOutlinedIcon from "@material-ui/icons/ArchiveOutlined";
 
 class Notes extends React.Component {
   render() {
@@ -28,6 +31,12 @@ class Notes extends React.Component {
           <Grid container spacing={2} className="note-row">
             <Grid item md={3}>
               <Card elevation={3} variant="outlined" className="note-card">
+              <CardHeader
+                  title={
+                    <Typography variant="h6">{"Title of the note"}</Typography>
+                  }
+                  className="card-title"
+                />
                 <List className="note-content checklist">
                   {[0, 1, 2, 3, 4, 5, 6].map((value) => {
                     const labelId = `checkbox-list-label-${value}`;
@@ -64,10 +73,90 @@ class Notes extends React.Component {
                     );
                   })}
                 </List>
+                <CardActions className="note-actions">
+                  <Grid container className="note-action-buttons">
+                    <Grid item md={2}>
+                      <IconButton
+                        className="button"
+                        color="inherit"
+                        aria-label="open drawer"
+                        edge="start"
+                      >
+                        <AddAlertOutlinedIcon className="menu-icon" />
+                      </IconButton>
+                    </Grid>
+                    <Grid item md={2}>
+                      <IconButton
+                        className="button"
+                        color="inherit"
+                        aria-label="open drawer"
+                        edge="start"
+                      >
+                        <PersonAddOutlinedIcon className="menu-icon" />
+                      </IconButton>
+                    </Grid>
+                    <Grid item md={2}>
+                      <IconButton
+                        className="button"
+                        color="inherit"
+                        aria-label="open drawer"
+                        edge="start"
+                      >
+                        <ColorLensOutlinedIcon className="menu-icon" />
+                      </IconButton>
+                    </Grid>
+                    <Grid item md={2}>
+                      <IconButton
+                        className="button"
+                        color="inherit"
+                        aria-label="open drawer"
+                        edge="start"
+                      >
+                        <CropOriginalIcon className="menu-icon" />
+                      </IconButton>
+                    </Grid>
+                    <Grid item md={2}>
+                      <IconButton
+                        className="button"
+                        color="inherit"
+                        aria-label="open drawer"
+                        edge="start"
+                      >
+                        <ArchiveOutlinedIcon className="menu-icon" />
+                      </IconButton>
+                    </Grid>
+                    <Grid item md={2}>
+                      <IconButton
+                        className="button"
+                        color="inherit"
+                        aria-label="open drawer"
+                        edge="start"
+                      >
+                        <MoreVertIcon className="menu-icon" />
+                      </IconButton>
+                    </Grid>
+                  </Grid>
+                </CardActions>
               </Card>
             </Grid>
             <Grid item md={3}>
               <Card elevation={3} variant="outlined" className="note-card">
+                <IconButton
+                  className="select-button"
+                  color="inherit"
+                  aria-label="open drawer"
+                  edge="start"
+                >
+                  <CheckCircleIcon className="menu-icon" />
+                </IconButton>
+                <IconButton
+                  className="pin-button"
+                  color="inherit"
+                  aria-label="open drawer"
+                  edge="start"
+                >
+                  <RoomOutlinedIcon className="menu-icon" />
+                </IconButton>
                 <CardHeader
                   title={
                     <Typography variant="h6">{"Title of the note"}</Typography>
@@ -167,6 +256,22 @@ class Notes extends React.Component {
             </Grid>
             <Grid item md={3}>
               <Card elevation={3} variant="outlined" className="note-card">
+                <IconButton
+                  className="select-button"
+                  color="inherit"
+                  aria-label="open drawer"
+                  edge="start"
+                >
+                  <CheckCircleIcon className="menu-icon" />
+                </IconButton>
+                <IconButton
+                  className="pin-button"
+                  color="inherit"
+                  aria-label="open drawer"
+                  edge="start"
+                >
+                  <RoomOutlinedIcon className="menu-icon" />
+                </IconButton>
                 <CardHeader
                   title={
                     <Typography variant="h6">{"Title of the note"}</Typography>
@@ -266,6 +371,22 @@ class Notes extends React.Component {
             </Grid>
             <Grid item md={3}>
               <Card elevation={3} variant="outlined" className="note-card">
+                <IconButton
+                  className="select-button"
+                  color="inherit"
+                  aria-label="open drawer"
+                  edge="start"
+                >
+                  <CheckCircleIcon className="menu-icon" />
+                </IconButton>
+                <IconButton
+                  className="pin-button"
+                  color="inherit"
+                  aria-label="open drawer"
+                  edge="start"
+                >
+                  <RoomOutlinedIcon className="menu-icon" />
+                </IconButton>
                 <CardHeader
                   title={
                     <Typography variant="h6">{"Title of the note"}</Typography>
@@ -365,6 +486,22 @@ class Notes extends React.Component {
             </Grid>
             <Grid item md={3}>
               <Card elevation={3} variant="outlined" className="note-card">
+                <IconButton
+                  className="select-button"
+                  color="inherit"
+                  aria-label="open drawer"
+                  edge="start"
+                >
+                  <CheckCircleIcon className="menu-icon" />
+                </IconButton>
+                <IconButton
+                  className="pin-button"
+                  color="inherit"
+                  aria-label="open drawer"
+                  edge="start"
+                >
+                  <RoomOutlinedIcon className="menu-icon" />
+                </IconButton>
                 <CardHeader
                   title={
                     <Typography variant="h6">{"Title of the note"}</Typography>
@@ -464,6 +601,22 @@ class Notes extends React.Component {
             </Grid>
             <Grid item md={3}>
               <Card elevation={3} variant="outlined" className="note-card">
+                <IconButton
+                  className="select-button"
+                  color="inherit"
+                  aria-label="open drawer"
+                  edge="start"
+                >
+                  <CheckCircleIcon className="menu-icon" />
+                </IconButton>
+                <IconButton
+                  className="pin-button"
+                  color="inherit"
+                  aria-label="open drawer"
+                  edge="start"
+                >
+                  <RoomOutlinedIcon className="menu-icon" />
+                </IconButton>
                 <CardHeader
                   title={
                     <Typography variant="h6">{"Title of the note"}</Typography>
@@ -563,6 +716,22 @@ class Notes extends React.Component {
             </Grid>
             <Grid item md={3}>
               <Card elevation={3} variant="outlined" className="note-card">
+                <IconButton
+                  className="select-button"
+                  color="inherit"
+                  aria-label="open drawer"
+                  edge="start"
+                >
+                  <CheckCircleIcon className="menu-icon" />
+                </IconButton>
+                <IconButton
+                  className="pin-button"
+                  color="inherit"
+                  aria-label="open drawer"
+                  edge="start"
+                >
+                  <RoomOutlinedIcon className="menu-icon" />
+                </IconButton>
                 <CardHeader
                   title={
                     <Typography variant="h6">{"Title of the note"}</Typography>
@@ -662,6 +831,22 @@ class Notes extends React.Component {
             </Grid>
             <Grid item md={3}>
               <Card elevation={3} variant="outlined" className="note-card">
+                <IconButton
+                  className="select-button"
+                  color="inherit"
+                  aria-label="open drawer"
+                  edge="start"
+                >
+                  <CheckCircleIcon className="menu-icon" />
+                </IconButton>
+                <IconButton
+                  className="pin-button"
+                  color="inherit"
+                  aria-label="open drawer"
+                  edge="start"
+                >
+                  <RoomOutlinedIcon className="menu-icon" />
+                </IconButton>
                 <CardHeader
                   title={
                     <Typography variant="h6">{"Title of the note"}</Typography>
@@ -759,141 +944,7 @@ class Notes extends React.Component {
                 </CardActions>
               </Card>
             </Grid>
-            <Grid item md={3}>
-              <Card elevation={3} variant="outlined" className="note-card">
-                <CardHeader
-                  title={
-                    <Typography variant="h6">{"Title of the note"}</Typography>
-                  }
-                  className="card-title"
-                />
-                <Typography variant="body2" className="note-content">
-                  Nunc congue posuere lorem id mattis. Mauris id dolor ex.
-                  Mauris rhoncus id eros eu fermentum. Morbi semper, dui sit
-                  amet dapibus sollicitudin, massa lectus convallis metus, et
-                  mollis felis nunc vitae sem. Ut sit amet scelerisque urna.
-                  Nunc euismod quam in ex dignissim, et suscipit mi tempus.
-                  Maecenas sed ligula sit amet sem semper fermentum. Etiam
-                  dapibus aliquet enim, et sodales purus eleifend sit amet.
-                  Etiam pulvinar, felis a vulputate tempus, eros enim ultricies
-                  ipsum, id gravida tellus ante accumsan libero. Mauris
-                  fermentum lorem turpis, sit amet mattis est ultrices ut. Sed
-                  aliquam urna eu felis hendrerit, at blandit libero hendrerit.
-                  Cras ornare blandit elementum. Duis in fermentum ligula. Nunc
-                  id pharetra neque, quis dignissim nisi. Suspendisse mollis
-                  risus nec viverra viverra. Nulla fermentum lobortis ante,
-                  vitae lacinia libero pellentesque in. Vestibulum pretium,
-                  risus in dignissim condimentum, purus mi volutpat justo, id
-                  maximus urna neque vitae lacus. Suspendisse in velit malesuada
-                  velit rutrum venenatis sed a nisi. Nulla volutpat tortor sem,
-                  quis ultrices ligula ultrices in. Nunc eget ultrices quam.
-                  Nullam ut malesuada metus. Sed bibendum, dolor a fringilla
-                  vulputate, nunc augue gravida ipsum, eu pulvinar erat enim sed
-                  lacus. Maecenas vel dolor consequat, ullamcorper velit sed,
-                  suscipit ante. Aliquam erat volutpat
-                </Typography>
-                <CardActions className="note-actions">
-                  <Grid container className="note-action-buttons">
-                    <Grid item md={2}>
-                      <IconButton
-                        className="button"
-                        color="inherit"
-                        aria-label="open drawer"
-                        edge="start"
-                      >
-                        <AddAlertOutlinedIcon className="menu-icon" />
-                      </IconButton>
-                    </Grid>
-                    <Grid item md={2}>
-                      <IconButton
-                        className="button"
-                        color="inherit"
-                        aria-label="open drawer"
-                        edge="start"
-                      >
-                        <PersonAddOutlinedIcon className="menu-icon" />
-                      </IconButton>
-                    </Grid>
-                    <Grid item md={2}>
-                      <IconButton
-                        className="button"
-                        color="inherit"
-                        aria-label="open drawer"
-                        edge="start"
-                      >
-                        <ColorLensOutlinedIcon className="menu-icon" />
-                      </IconButton>
-                    </Grid>
-                    <Grid item md={2}>
-                      <IconButton
-                        className="button"
-                        color="inherit"
-                        aria-label="open drawer"
-                        edge="start"
-                      >
-                        <CropOriginalIcon className="menu-icon" />
-                      </IconButton>
-                    </Grid>
-                    <Grid item md={2}>
-                      <IconButton
-                        className="button"
-                        color="inherit"
-                        aria-label="open drawer"
-                        edge="start"
-                      >
-                        <ArchiveOutlinedIcon className="menu-icon" />
-                      </IconButton>
-                    </Grid>
-                    <Grid item md={2}>
-                      <IconButton
-                        className="button"
-                        color="inherit"
-                        aria-label="open drawer"
-                        edge="start"
-                      >
-                        <MoreVertIcon className="menu-icon" />
-                      </IconButton>
-                    </Grid>
-                  </Grid>
-                </CardActions>
-              </Card>
             </Grid>
-            <Grid item md={3}>
-              <Card elevation={3} variant="outlined" className="note-card">
-                <CardHeader
-                  title={
-                    <Typography variant="h6">{"Title of the note"}</Typography>
-                  }
-                  className="card-title"
-                />
-                <Typography variant="body2" className="note-content">
-                  Nunc congue posuere lorem id mattis. Mauris id dolor ex.
-                  Mauris rhoncus id eros eu fermentum. Morbi semper, dui sit
-                  amet dapibus sollicitudin, massa lectus convallis metus, et
-                  mollis felis nunc vitae sem. Ut sit amet scelerisque urna.
-                  Nunc euismod quam in ex dignissim, et suscipit mi tempus.
-                  Maecenas sed ligula sit amet sem semper fermentum. Etiam
-                  dapibus aliquet enim, et sodales purus eleifend sit amet.
-                  Etiam pulvinar, felis a vulputate tempus, eros enim ultricies
-                  ipsum, id gravida tellus ante accumsan libero. Mauris
-                  fermentum lorem turpis, sit amet mattis est ultrices ut. Sed
-                  aliquam urna eu felis hendrerit, at blandit libero hendrerit.
-                  Cras ornare blandit elementum. Duis in fermentum ligula. Nunc
-                  id pharetra neque, quis dignissim nisi. Suspendisse mollis
-                  risus nec viverra viverra. Nulla fermentum lobortis ante,
-                  vitae lacinia libero pellentesque in. Vestibulum pretium,
-                  risus in dignissim condimentum, purus mi volutpat justo, id
-                  maximus urna neque vitae lacus. Suspendisse in velit malesuada
-                  velit rutrum venenatis sed a nisi. Nulla volutpat tortor sem,
-                  quis ultrices ligula ultrices in. Nunc eget ultrices quam.
-                  Nullam ut malesuada metus. Sed bibendum, dolor a fringilla
-                  vulputate, nunc augue gravida ipsum, eu pulvinar erat enim sed
-                  lacus. Maecenas vel dolor consequat, ullamcorper velit sed,
-                  suscipit ante. Aliquam erat volutpat
-                </Typography>
-              </Card>
-            </Grid>
-          </Grid>
         </Grid>
       </Grid>
     );
