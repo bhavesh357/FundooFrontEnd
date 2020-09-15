05595f97e1d878fac36e5cbbec811ce823a73184
bhavesh357 <bkadam357@gmail.com>
2020-09-15 22:37:40 +0530

    ADDED: tests to check sign in component

--- src/Tests/SignIn.test.js
+++ src/Tests/SignIn.test.js
@@ -2,20 +2,39 @@ import React from 'react';
 import Enzyme, { render, shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import SignIn from './../Components/pages/SignIn';
-import { TextField } from '@material-ui/core';
-import { BrowserRouter } from 'react-router-dom';
+import { Button, Snackbar } from '@material-ui/core';
 
-
-React.useLayoutEffect = React.useEffect;
 Enzyme.configure({
     adapter: new Adapter()
 });
 
 describe('Sign In', () => {
-    it('Should be true', () => {
-        const wrapper = shallow(<SignIn />);
-        const fields = wrapper.find('Hello');
-        console.log(fields);
-        expect(fields).to.have.lengthOf(1);
+
+    let wrapper;
+    let button;
+    beforeEach(()=>{
+        wrapper = shallow(<SignIn />);
+        button = wrapper.find(Button);
+    })
+
+    it('when proper email and password should be valid', () => {
+        wrapper.setState({
+            email:'bhavesh357357@gmail.com',
+            password: 'Testing@357'
+        })
+        button.simulate('click');
+        expect(wrapper.state('passwordInvalid')).toBe(false);
+        expect(wrapper.state('emailInvalid')).toBe(false);
+    });
+
+    it('when improper email and password should be invalid', () => {
+        const button = wrapper.find(Button);
+        wrapper.setState({
+            email:'bhavesh357357gmail',
+            password: 'testing@357'
+        })
+        button.simulate('click');
+        expect(wrapper.state('passwordInvalid')).toBe(true);
+        expect(wrapper.state('emailInvalid')).toBe(true);
     });
 });
